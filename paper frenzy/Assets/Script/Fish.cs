f3e599991daea5de295770916485d567d5b3438e
alvian0 <reaperaz84@gmail.com>
2020-12-16 11:09:28 +0700

    Prototype

---
+++ paper frenzy/Assets/Script/Fish.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    public float Speed, maxY, minY, maxX, minX;
+
+    Vector2 poss;
+    float currentXpos;
+
+    void Start()
+    {
+        currentXpos = transform.position.x;
+        poss = new Vector2(Random.Range(minX, maxY), Random.Range(minY, maxY));
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, poss, Speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, poss) < 0.5f)
+        {
+            poss = new Vector2(Random.Range(minX, maxY), Random.Range(minY, maxY));
+        }
+
+        if (transform.position.x > currentXpos)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            currentXpos = transform.position.x;
+        }
+
+        if (transform.position.x < currentXpos)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            currentXpos = transform.position.x;
+        }
+    }
+}
---
+++ paper frenzy/Assets/Script/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed;
+    public float size = 1f;
+
+    Rigidbody2D rb;
+    Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movement = move.normalized * Speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Fish")
+        {
+            Destroy(collision.gameObject);
+            transform.localScale += new Vector3(size, size, 0);
+        }
+    }
+}
